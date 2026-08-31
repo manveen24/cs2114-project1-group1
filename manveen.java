aa397ff2db0f98cf7a1b221b1642bb12d24405be
manveen24 <manveen@vt.edu>
2026-08-31 15:36:36 -0400

    Update output message in Manveen class

--- manveen.java
+++ manveen.java
@@ -1,5 +1,5 @@
 public class Manveen {
     public static void main(String[] args) {
-        System.out.println("Your Name is on the team.");
+        System.out.println("Your Manveen is on the team.");
     }
 }
